adcf0e7d345ba6fff889a0b2cc0d7ba83eb2bae7
triniz <triki.nizar@gmail.com>
2019-04-28 12:49:52 -0400

    set email php form

--- contact.php
+++ contact.php
@@ -1,51 +1,18 @@
 <?php
-
-#Receive user input
-$email_address = $_POST['email'];
-$feedback = $_POST['message'];
-
-
-
-#Filter user input
-function filter_email_header($form_field) {
-return preg_replace('/[\0\n\r\|\!\/\<\>\^\$\%\*\&]+/','',$form_field);
-}
-
-$email_address = filter_email_header($email_address);
-
-// Headers
-$headers  = 'MIME-Version: 1.0' . "\r\n"
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: ' . $email_address . "\r\n";
-
-#Send email
-$sent = mail('triki.nizar@gmail.com', 'Nouveau message du site web', $feedback, $headers);
-
-#Thank user or notify them of a problem
-if ($sent) {
-
-?><html>
-<head>
-<title>Merci</title>
-</head>
-<body>
-<h1>Merci</h1>
-<p>Merci pour votre message.</p>
-</body>
-</html>
-<?php
-
-} else {
-
-?><html>
-<head>
-<title>Erreur</title>
-</head>
-<body>
-<h1>Erreur</h1>
-<p>Nous n'avons pas pu envoyer votre email.</p>
-</body>
-</html>
-<?php
-}
+if (isset($_POST["email"])) {
+    $to_email = 'triki.nizar@gmail.com';
+    $subject = $_POST["subject"];
+    $headers = array(
+	    'From' => 'contact@hac-consulting.net',
+	    'Reply-To' => 'triki.nizar@gmail.com',
+	    'X-Mailer' => 'PHP/' . phpversion()
+	);
+    $body = $_POST["message"];
+ 
+    if ( mail($to_email, $subject, $body, $headers)) {
+      echo("Email successfully sent to $to_email...");
+    } else {
+      echo("Email sending failed...");
+    }
+  }
 ?>
